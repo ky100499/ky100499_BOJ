0df6de133726b55e816e9526ebc3f6bbb4f4f3a1
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-03-06 23:03:39 +0900

    Prob. 1017

--- 1000~1999/1017.py
+++ 1000~1999/1017.py
@@ -11,20 +11,37 @@ def isPrime(n):
 
 primes = [i for i in range(2, 2000) if isPrime(i)]
 
-def divide(l):
-    if len(l) == 2: return sum(l) in primes
-    n = l.pop()
-    for i in range(len(l)):
-        if n + l[i] in primes and divide(l[:i] + l[i+1:]):
-            return True
-    else: return False
+def mat(idx, A, B, matched, visited):
+    if visited[idx]: return False
+    visited[idx] = True
+    for i, b in enumerate(B):
+        if A[idx] + b in primes:
+            if matched[1][i] is False or mat(matched[1][i], A, B, matched, visited):
+                matched[0][idx] = i
+                matched[1][i] = idx
+                return True
+    return False
 
 N = int(input())
 nums = list(map(int, input().split()))
-n = nums.pop(0)
-ans = []
-for i in range(N-1):
-    if n + nums[i] in primes and divide(nums[:i] + nums[i+1:]):
-        ans.append(nums[i])
+A = list(filter(lambda x: x % 2 == nums[0] % 2, nums))
+B = list(filter(lambda x: x % 2 != nums[0] % 2, nums))
 
-print(' '.join(map(str, sorted(ans))) if len(ans) else -1)
+a = A.pop(0)
+
+try:
+    ans = []
+    for i, b in enumerate(B):
+        if a + b in primes:
+            matched = [[False for _ in range(N//2-1)] for _ in range(2)]
+            cnt = 0
+            for j in range(N//2-1):
+                visited = [False for _ in range(N//2-1)]
+                if mat(j, A, B[:i]+B[i+1:], matched, visited):
+                    cnt += 1
+            if cnt == N//2-1:
+                ans.append(b)
+
+    print(' '.join(map(str, sorted(ans))) if len(ans) else -1)
+except IndexError:
+    print(-1)
