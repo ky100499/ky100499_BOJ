66851d4b21510221d7551bee1dba560dc2ba514b
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-04-05 13:51:29 +0900

    Update 6209.py

--- probs/6209.py
+++ probs/6209.py
@@ -13,4 +13,16 @@ ipt.sort()
 diff = []
 for i in range(n+1):
     diff.append(ipt[i+1] - ipt[i])
-print(diff)
+
+for _ in range(m):
+    i = diff.index(min(diff))
+    if i == 0:
+        diff = [diff[0]+diff[1]] + diff[2:]
+    elif i == len(diff)-1:
+        diff = diff[:-2] + [diff[-2]+diff[-1]]
+    elif diff[i-1] < diff[i+1]:
+        diff = diff[:i-1] + [diff[i-1]+diff[i]] + diff[i+1:]ㄷㅌ
+    else:
+        diff = diff[:i] + [diff[i]+diff[i+1]] + diff[i+2:]
+
+print(min(diff))
