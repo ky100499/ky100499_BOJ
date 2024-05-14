82abb02fcd5e761ecce02797a345b016ae9f8caf
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-05-14 13:20:20 +0900

    hcpc 26156 time complexity improve

--- probs/26156(hcpc)_2.py
+++ probs/26156(hcpc)_2.py
@@ -1,7 +1,20 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
 
 input = open('input.txt', 'r').readline
+
+def power(b, p):
+    if p == 0:
+        return 1
+    if p == 1:
+        return b
+
+    x = power(b, p//2) % DIV
+    if p % 2:
+        return (x * x * b) % DIV
+    else:
+        return (x * x) % DIV
 
 DIV = 10**9+7
 
@@ -12,7 +25,7 @@ cnt = [0]*4
 
 for i, c in enumerate(S):
     if c == 'R':
-        cnt[0] = (cnt[0] + 2 ** i) % DIV
+        cnt[0] = (cnt[0] + power(2, i)) % DIV
     elif c == 'O':
         cnt[1] = (cnt[1] + cnt[0]) % DIV
     elif c == 'C':
