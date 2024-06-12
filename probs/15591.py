81ec313a857bfa4bcfa11808120061724895b1cb
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-06-12 17:18:20 +0900

    240612

--- probs/15591.py
+++ probs/15591.py
@@ -1,28 +1,31 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
 
 input = open('input.txt', 'r').readline
 
-INF = int(1e10)
+from collections import deque
 
 N, Q = map(int, input().split())
-dist = [[INF]*(N+1) for _ in range(N+1)]
+edges = [{} for _ in range(N+1)]
 for _ in range(N-1):
     p, q, r = map(int, input().split())
-    dist[p][q] = r
-    dist[q][p] = r
-
-print(*dist, sep='\n')
-
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(i+1, N+1):
-            if dist[i][k] != INF and dist[k][j] != INF:
-                dist[i][j] = dist[j][i] = min(dist[i][j], dist[i][k], dist[k][j])
-
-print(*dist, sep='\n')
+    edges[p][q] = r
+    edges[q][p] = r
 
 for _ in range(Q):
     k, v = map(int, input().split())
-    print(dist[v])
+
+    ans = 0
+    visited = [0]*(N+1)
+    visited[v] = 1
+    q = deque([v])
+    while q:
+        v = q.popleft()
+
+        for n, r in edges[v].items():
+            if r >= k and not visited[n]:
+                visited[n] = 1
+                ans += 1
+                q.append(n)
+
+    print(ans)
