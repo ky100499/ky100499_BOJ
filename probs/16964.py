483a36358d27d52919e2cf093159b7a5c726cb54
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-08-16 19:06:01 +0900

    240816 (2)

--- probs/16964.py
+++ probs/16964.py
@@ -6,35 +6,18 @@ input = open('input.txt', 'r').readline
 from collections import deque
 
 N = int(input())
-G = [[] for _ in range(N+1)]
+G = [set() for _ in range(N+1)]
 for _ in range(N-1):
     a, b = map(int, input().split())
-    G[a].append(b)
-    G[b].append(a)
-
-T = [[] for _ in range(N+1)]
-V = [0]*(N+1)
-q = deque([1])
-while q:
-    n = q.popleft()
-
-    if V[n]:
-        continue
-    V[n] = 1
-
-    for c in G[n]:
-        if not V[c]:
-            T[n].append(c)
-            q.append(c)
+    G[a].add(b)
+    G[b].add(a)
 
 A = list(map(int, input().split()))
 if A[0] == 1:
     S = deque([1])
-    V = [0]*(N+1)
-    V[1] = 1
     for a in A[1:]:
         while S:
-            if T[S[-1]]:
+            if G[S[-1]]:
                 break
             else:
                 S.pop()
@@ -42,9 +25,9 @@ if A[0] == 1:
             print(0)
             break
 
-        if a in T[S[-1]]:
-            T[S[-1]].remove(a)
-            V[a] = 1
+        if a in G[S[-1]]:
+            G[S[-1]].discard(a)
+            G[a].discard(S[-1])
             S.append(a)
         else:
             print(0)
