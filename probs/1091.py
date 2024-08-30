67b784476e98e7fe43b3c35bf067f548470d15d7
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-08-30 21:03:31 +0900

    240830 (2)

--- probs/1091.py
+++ probs/1091.py
@@ -9,11 +9,31 @@ def shuffle(P, S):
         ret[s] = P[i]
     return ret
 
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+def lcm(a, b):
+    return a*b//gcd(a, b)
+
 N = int(input())
 P = list(map(int, input().split()))
 S = list(map(int, input().split()))
 
+L = [0]*N
+loop = 1
 for i in range(N):
+    if L[i]:
+        continue
+    a, k = S[i], 1
+    while a != i:
+        L[a] = 1
+        a = S[a]
+        k += 1
+    loop = lcm(loop, k)
+
+for i in range(loop):
     for j in range(N):
         if P[j] != j%3:
             break
