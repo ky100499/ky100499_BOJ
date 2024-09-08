c6d740c5e653686264e93067b74b5247c458a316
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2024-09-08 14:46:35 +0900

    240908 (2)

--- probs/16946.py
+++ probs/16946.py
@@ -3,49 +3,43 @@ input = sys.stdin.readline
 
 input = open('input.txt', 'r').readline
 
-def dfs(i, j):
-    dp[i][j] = 1
-    c = 0
-    for di, dj in [[-1,0],[0,-1],[1,0],[0,1]]:
-        ni, nj = i+di, j+dj
-        if 0 <= ni < N and 0 <= nj < M and B[ni][nj] == 0 and dp[ni][nj] == 0:
-            c += dfs(ni, nj)
-    dp[i][j] = 0
-
-    return c+1
-
-def prop(i, j, c, v):
-    dp[i][j] = c
-    V[i][j] = v
-    for di, dj in [[-1,0],[0,-1],[1,0],[0,1]]:
-        ni, nj = i+di, j+dj
-        if 0 <= ni < N and 0 <= nj < M and B[ni][nj] == 0 and dp[ni][nj] == 0:
-            prop(ni, nj, c, v)
+from collections import deque, defaultdict as dd
 
 N, M = map(int, input().split())
 B = [list(map(int, input().rstrip())) for _ in range(N)]
-dp = [[0]*M for _ in range(N)]
 V = [[0]*M for _ in range(N)]
-v = 1
+A = dd(int)
 
+v = 1
 for i in range(N):
     for j in range(M):
-        if B[i][j] == 0 and dp[i][j] == 0:
-            prop(i, j, dfs(i, j), v)
+        if not B[i][j] and not V[i][j]:
+            q = deque([(i, j)])
+            while q:
+                x, y = q.popleft()
+
+                if V[x][y]:
+                    continue
+                V[x][y] = v
+                A[v] += 1
+
+                for dx, dy in ((-1,0),(0,-1),(1,0),(0,1)):
+                    nx, ny = x+dx, y+dy
+                    if 0 <= nx < N and 0 <= ny < M and not B[nx][ny] and not V[nx][ny]:
+                        q.append((nx, ny))
             v += 1
 
 for i in range(N):
     for j in range(M):
-        if B[i][j] == 1:
-            dp[i][j] = 1
-            visited = set()
-            for di, dj in [[-1,0],[0,-1],[1,0],[0,1]]:
-                ni, nj = i+di, j+dj
-                if 0 <= ni < N and 0 <= nj < M and B[ni][nj] == 0 and V[ni][nj] not in visited:
-                    dp[i][j] += dp[ni][nj]
-                    visited.add(V[ni][nj])
+        if B[i][j]:
+            area = set()
+            V[i][j] = 1
+            for dx, dy in ((-1,0),(0,-1),(1,0),(0,1)):
+                nx, ny = i+dx, j+dy
+                if 0 <= nx < N and 0 <= ny < M and not B[nx][ny]:
+                    area.add(V[nx][ny])
+            for a in area:
+                V[i][j] += A[a]
 
 for i in range(N):
-    for j in range(M):
-        print(dp[i][j] * B[i][j], end='')
-    print()
+    print(*[B[i][j]*V[i][j]%10 for j in range(M)], sep='')
