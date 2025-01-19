2525d2815fd326c9bcc8413a729d9aeabdb7d6e8
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2025-01-19 18:29:49 +0900

    250119

--- probs/17412.cpp
+++ probs/17412.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-const int MAX = 400*2,
-          INF = 1e9;
+const int MAX = 400;
 
 int F[MAX+1][MAX+1],
     C[MAX+1][MAX+1],
@@ -21,21 +20,14 @@ int main()
     memset(C, 0, sizeof C);
 
     int N, P; cin >> N >> P;
-    for (int i = 1; i <= N; i++) {
-        C[i][i+N] = 1;
-        G[i].push_back(i+N);
-        G[i+N].push_back(i);
-    }
-    C[1][1+N] = INF;
     while (P--) {
         int u, v; cin >> u >> v;
-        C[u+N][v] = C[v+N][u] = 1;
-        G[u+N].push_back(v);
-        G[v].push_back(u+N);
-        G[v+N].push_back(u);
-        G[u].push_back(v+N);
+        C[u][v] = 1;
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
 
+    int ans = 0;
     while (1) {
         memset(pre, -1, sizeof pre);
         queue<int> q({1});
@@ -58,8 +50,9 @@ int main()
             F[pre[u]][u] += 1;
             F[u][pre[u]] -= 1;
         }
+        ans += 1;
     }
-    cout << F[1][1+N] << '\n';
+    cout << ans << '\n';
 
     return 0;
 }
