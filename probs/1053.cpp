d2d9847963d09ea9155aa1f3706de84a5c366887
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2025-01-28 20:04:28 +0900

    250128 (2)

--- probs/1053.cpp
+++ probs/1053.cpp
@@ -5,20 +5,23 @@ using namespace std;
 const int MAX = 50,
           INF = 1e9;
 
-int dp[MAX][MAX];
+int dp[MAX][MAX], len;
 
-int dfs(int s, int e, string S)
+int solve(string S)
 {
-    if (e <= s) return 0;
+    fill(&dp[0][0], &dp[len][0], INF);
+    for (int i = 0; i < len; i++) dp[i][i] = 0;
+    for (int i = 1; i < len; i++) dp[i-1][i] = S[i-1] != S[i];
 
-    if (dp[s][e] == INF) {
-        dp[s][e] = min(
-            dfs(s+1, e-1, S) + (S[s] != S[e]),
-            min(dfs(s, e-1, S), dfs(s+1, e, S)) + 1
-        );
+    for (int l = 0; l < len-1; l++) {
+        for (int s = 0; s+l < len; s++) {
+            if (s > 0) dp[s-1][s+l] = min(dp[s-1][s+l], dp[s][s+l]+1);
+            if (s+l+1 < len) dp[s][s+l+1] = min(dp[s][s+l+1], dp[s][s+l]+1);
+            if (s > 0 && s+l+1 < len) dp[s-1][s+l+1] = min(dp[s-1][s+l+1], dp[s][s+l]+(S[s-1] != S[s+l+1]));
+        }
     }
 
-    return dp[s][e];
+    return dp[0][len-1];
 }
 
 int main()
@@ -29,16 +32,14 @@ int main()
     freopen("input.txt", "r", stdin);
 
     string S; cin >> S;
-    int len = S.size();
+    len = S.size();
 
-    fill(&dp[0][0], &dp[len][0], INF);
-    int ans = dfs(0, len-1, S);
+    int ans = solve(S);
     for (int i = 0; i < len; i++) {
         for (int j = i+1; j < len; j++) {
             if (S[i] != S[j]) {
                 swap(S[i], S[j]);
-                fill(&dp[0][0], &dp[len][0], INF);
-                ans = min(ans, dfs(0, len-1, S)+1);
+                ans = min(ans, solve(S)+1);
                 swap(S[i], S[j]);
             }
         }
