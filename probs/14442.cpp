08fa0fd0be933526f409882dbd9513c0197b8952
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2025-06-13 20:36:16 +0900

    250613

---
+++ probs/14442.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define in(x, a, b) (a <= x && x < b)
+
+using namespace std;
+using pi = pair<int, int>;
+using ti = tuple<int, int, int>;
+using qi = tuple<int, int, int, int>;
+
+const int MAX = 1000,
+          INF = 1e9;
+
+int A[MAX][MAX],
+    D[MAX][MAX][11],
+    mv[4][2] = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("input.txt", "r", stdin);
+
+    int N, M, K; cin >> N >> M >> K;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            char c; cin >> c;
+            A[i][j] = c - '0';
+        }
+    }
+    fill(&D[0][0][0], &D[N][0][0], INF);
+
+    queue<qi> q; q.emplace(0, 0, 1, 0); D[0][0][0] = 1;
+    int ans = -1;
+    while (!q.empty()) {
+        auto [x, y, d, k] = q.front(); q.pop();
+
+        if (D[x][y][k] < d) continue;
+        if (x == N-1 && y == M-1) {
+            ans = d;
+            break;
+        }
+
+        for (auto [dx, dy] : mv) {
+            int nx = x+dx, ny = y+dy;
+            if (!in(nx, 0, N) || !in(ny, 0, M)) continue;
+
+            if (!A[nx][ny] && D[nx][ny][k] > d+1) {
+                D[nx][ny][k] = d+1;
+                q.emplace(nx, ny, d+1, k);
+            }
+            if (A[nx][ny] && k < K && D[nx][ny][k+1] > d+1) {
+                D[nx][ny][k+1] = d+1;
+                q.emplace(nx, ny, d+1, k+1);
+            }
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
