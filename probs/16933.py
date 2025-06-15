df758e1c87a04cbe48135e9838bd5ea963aa9976
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2025-06-16 00:15:39 +0900

    250614(2)

--- probs/16933.py
+++ probs/16933.py
@@ -10,38 +10,30 @@ INF = 10**8
 N, M, K = map(int, input().split())
 B = [list(map(int, input().strip())) for _ in range(N)]
 
-dist = [[[INF]*(K+1) for _ in range(M)] for _ in range(N)]
-dist[0][0][0] = 1
+V = [[[0]*(K+1) for _ in range(M)] for _ in range(N)]
+V[0][0][0] = 1
 
-q1, q2 = deque([(1, 0, 0, 0)]), deque()
-while q1 or q2:
-    if not q1:
-        d, i, j, k = q2.popleft()
-    elif not q2:
-        d, i, j, k = q1.popleft()
-    elif q1[0] > q2[0]:
-        d, i, j, k = q2.popleft()
-    else:
-        d, i, j, k = q1.popleft()
-
-    if dist[i][j][k] < d:
-        continue
+q = deque([(1,0,0,0)])
+while q:
+    d, i, j, k = q.popleft()
 
     if (i, j) == (N-1, M-1):
         print(d)
         break
 
+    stay = 0
     for di, dj in [(-1,0), (0,-1), (1,0), (0,1)]:
         ni, nj = i+di, j+dj
         if 0 <= ni < N and 0 <= nj < M:
-            if B[ni][nj] == 0 and dist[ni][nj][k] > (nd := dist[i][j][k]+1):
-                dist[ni][nj][k] = nd
-                q1.append((nd, ni, nj, k))
-            elif k < K and B[ni][nj] == 1 and d % 2 and dist[ni][nj][k+1] > (nd := dist[i][j][k]+1):
-                dist[ni][nj][k+1] = nd
-                q1.append((nd, ni, nj, k+1))
-            elif k < K and B[ni][nj] == 1 and d % 2 == 0 and dist[ni][nj][k+1] > (nd := dist[i][j][k]+2):
-                dist[ni][nj][k+1] = nd
-                q2.append((nd, ni, nj, k+1))
+            if B[ni][nj] == 0 and not V[ni][nj][k]:
+                V[ni][nj][k] = 1
+                q.append((d+1, ni, nj, k))
+            if B[ni][nj] == 1 and k < K and not V[ni][nj][k+1]:
+                if d%2:
+                    V[ni][nj][k+1] = 1
+                    q.append((d+1, ni, nj, k+1))
+                elif not stay:
+                    stay = 1
+                    q.append((d+1, i, j, k))
 else:
     print(-1)
