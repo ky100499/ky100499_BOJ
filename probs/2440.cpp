69f45da9d796d05bdc0f5dae10830d13373f396b
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2025-09-23 01:09:16 +0900

    250922

---
+++ probs/2440.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("input.txt", "r", stdin);
+
+    int N; cin >> N;
+    while (N--) cout << string(N+1, '*') << '\n';
+
+    return 0;
+}
