6cfd30bcd41c7fa9409ae7360edc5634edac63a7
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2026-02-10 14:24:01 +0900

    260210

---
+++ probs/15827.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+using ssc = tuple<string, string, char>;
+
+map<string, pll> V, ans;
+map<string, ssc> M;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0) return a;
+    else return gcd(b, a%b);
+}
+
+pll rdc(pll a)
+{
+    auto [p, q] = a;
+    ll g = gcd(abs(p), abs(q));
+    p /= g; q /= g;
+    if (q < 0) p *= -1, q *= -1;
+    return {p, q};
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("input.txt", "r", stdin);
+
+    int N; cin >> N;
+    queue<string> q;
+    while (N--) {
+        string A; char o; cin >> A >> o;
+        if (o == 'E') {
+            int v; cin >> v;
+            V[A] = {v,1};
+        }
+        else {
+            string x, y; cin >> x >> y;
+            M[A] = {x, y, o};
+            q.push(A);
+        }
+        ans[A] = {0,1};
+    }
+
+    while (!q.empty()) {
+        string A = q.front(); q.pop();
+        auto [x, y, o] = M[A];
+
+        if (V.find(x) == V.end() || V.find(y) == V.end()) q.push(A);
+        else {
+            auto [p1, q1] = V[x];
+            auto [p2, q2] = V[y];
+            ll p, q;
+
+            if (o == 'A') {
+                p = p1*q2 + p2*q1;
+                q = q1*q2;
+            }
+            else if (o == 'S') {
+                p = p1*q2 - p2*q1;
+                q = q1*q2;
+            }
+            else if (o == 'M') {
+                p = p1*p2;
+                q = q1*q2;
+            }
+            else {
+                p = p1*q2;
+                q = q1*p2;
+            }
+
+            V[A] = rdc({p, q});
+        }
+    }
+
+    ans["HAPPY"] = {1,1};
+    q.emplace("HAPPY");
+    while (!q.empty()) {
+        auto A = q.front(); q.pop();
+        if (M.find(A) == M.end()) continue;
+
+        auto [x, y, o] = M[A];
+        if (o == 'A') {
+            ans[x] = ans[y] = ans[A];
+        }
+        else if (o == 'S') {
+            auto [p, q] = ans[A];
+            ans[x] = {p, q};
+            ans[y] = {-p, q};
+        }
+        else if (o == 'M') {
+            auto [p1, q1] = ans[A];
+            auto [p2, q2] = V[x];
+            auto [p3, q3] = V[y];
+
+            ans[x] = rdc({p1*p3, q1*q3});
+            ans[y] = rdc({p1*p2, q1*q2});
+        }
+        else {
+            auto [p1, q1] = ans[A];
+            auto [p2, q2] = V[x];
+            auto [p3, q3] = V[y];
+
+            auto [px, qx] = rdc({p1*q3, q1*p3});
+            auto [pt, qt] = rdc({p2*q3, q2*p3});
+            auto [py, qy] = rdc({-px*pt, qx*qt});
+
+            ans[x] = {px, qx};
+            ans[y] = {py, qy};
+        }
+
+        q.emplace(x);
+        q.emplace(y);
+    }
+
+    for (auto [k, v] : ans) {
+        auto [p, q] = v;
+        cout << k << ' ' << p << '/' << q << '\n';
+    }
+
+    return 0;
+}
